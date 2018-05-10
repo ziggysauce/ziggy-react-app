991e75a8ac6a11188ee4d8fe4a6cbff9d7efc771
ziggysauce <dtn3423@gmail.com>
2018-05-10 18:27:50 -0400

    Connect express server to frontend

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
+const { resolve } = require('path');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+const publicPath = resolve(__dirname, '..', 'client/dist');
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
+app.use(express.static(publicPath));
 
 app.listen(PORT, () => {
   console.log(`ziggy-react-app running on PORT: ${PORT}`);
